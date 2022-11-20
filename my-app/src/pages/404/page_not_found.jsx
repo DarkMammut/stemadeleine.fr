fb1b4542f13923bc88965a477b400bf53306685d
DarkMammut <seb.bellanger@gmail.com>
2022-11-20 21:53:51 +0100

    404 error

--- my-app/src/pages/404/page_not_found.jsx
+++ my-app/src/pages/404/page_not_found.jsx
@@ -1,40 +1,44 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./page_not_found.scss";
 
 function PageNotFound() {
   return (
     <main className="page">
-      <div id="background" />
-      <div className="top">
-        <h1 className="title">404</h1>
-        <h3 className="subhead">page non trouvée</h3>
+      <div id="page__background" />
+      <div className="page__top">
+        <h1 className="page__top__title">404</h1>
+        <h3 className="page__top__subhead">page non trouvée</h3>
       </div>
-      <div className="container">
-        <div className="ghost-copy">
-          <div className="one" />
-          <div className="two" />
-          <div className="three" />
-          <div className="four" />
-        </div>
-        <div className="ghost">
-          <div className="face">
-            <div className="eye" />
-            <div className="eye-right" />
-            <div className="mouth" />
+      <div className="page__container">
+        <div className="page__container__ghost">
+          <div className="page__container__ghost__hands">
+            <div className="page__container__ghost__hands--left" />
+            <div className="page__container__ghost__hands--right" />
+          </div>
+          <div className="page__container__ghost__face">
+            <div className="page__container__ghost__eyes">
+              <div className="page__container__ghost__eyes--left" />
+              <div className="page__container__ghost__eyes--right" />
+            </div>
+            <div className="page__container__ghost__mouth" />
+            <div className="page__container__ghost__feet">
+              <div className="page__container__ghost__feet--one" />
+              <div className="page__container__ghost__feet--two" />
+              <div className="page__container__ghost__feet--three" />
+              <div className="page__container__ghost__feet--four" />
+            </div>
           </div>
         </div>
-        <div className="shadow" />
+        <div className="page__container__shadow" />
       </div>
-      <div className="bottom">
-        <p className="text">Boo, looks like a ghost stole this page!</p>
-        <div className="buttons">
-          <button className="btn" type="button">
-            Back
-          </button>
-          <button className="btn" type="button">
-            Home
-          </button>
-        </div>
+      <div className="page__bottom">
+        <p className="page__bottom__text">
+          Boo, il semblerait que la page cherchée est introuvable !
+        </p>
+        <Link to="/" className="page__bottom__home">
+          Retourner à la page d&apos;accueil
+        </Link>
       </div>
     </main>
   );
