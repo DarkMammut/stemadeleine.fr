2af29111703a4b2ff7c5872aa89e8e612a5eac43
DarkMammut <seb.bellanger@gmail.com>
2022-12-04 16:57:32 +0100

    slider & responsive (mobilr) home

--- my-app/src/pages/bells/bells.jsx
+++ my-app/src/pages/bells/bells.jsx
@@ -6,6 +6,8 @@ import SECTIONS from "../../assets/bells_sections.json";
 document.title = "Les cloches";
 
 function Bells() {
+  const url = process.env.PUBLIC_URL;
+
   return (
     <main className="bells">
       <Helmet>
@@ -21,16 +23,44 @@ function Bells() {
         <section key={section.id} className="bells__section">
           <div className="bells__section__textarea">
             <h2 className="bells__section__textarea__title">{section.title}</h2>
-            <p className="bells__section__textarea__text">{section.text}</p>
+            <div className="bells__section__textarea__text">
+              {section.text.map((sectionText) => (
+                <p className="bells__section__textarea__text__paragraph" key={sectionText.id}>
+                  {sectionText.paragraph}
+                </p>
+              ))}
+            </div>
           </div>
-          <div className="bells__section__articles">
-            {section.article.map((art) => (
-              <article key={art.id} className="bells__section__articles__article">
-                <h3 className="bells__section__articles__article__title">{art.title}</h3>
-                <p className="bells__section__articles__article__text">{art.text}</p>
-              </article>
-            ))}
-          </div>
+          {section.images.length > 0 ? (
+            <div className="bells__section__images">
+              {section.images.map((image) => (
+                <div className="bells__section__images__image" key={image.id}>
+                  <div className="bells__section__images__image__container">
+                    <img src={url + image.url} title={image.title} alt={image.alt} />
+                  </div>
+                  <span>{image.title}</span>
+                </div>
+              ))}
+            </div>
+          ) : null}
+          {section.article.length > 0 ? (
+            <div className="bells__section__articles">
+              {section.article.map((art) => (
+                <article key={art.id} className="bells__section__articles__article">
+                  <h3 className="bells__section__articles__article__title">{art.title}</h3>
+                  <div className="bells__section__articles__article__text">
+                    {art.text.map((articleText) => (
+                      <p
+                        className="bells__section__articles__article__text__paragraph"
+                        key={articleText.id}>
+                        {articleText.paragraph}
+                      </p>
+                    ))}
+                  </div>
+                </article>
+              ))}
+            </div>
+          ) : null}
         </section>
       ))}
     </main>
