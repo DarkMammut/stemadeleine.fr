3c033fa3f0a90b6d16b9346bd924d57cb80d70e1
DarkMammut <seb.bellanger@gmail.com>
2023-05-13 09:07:42 +0200

    captcha

--- my-app/src/pages/contact/contact.jsx
+++ my-app/src/pages/contact/contact.jsx
@@ -1,39 +1,93 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import { Helmet } from "react-helmet";
+import ReCAPTCHA from "react-google-recaptcha";
+import axios from "axios";
 
 function Contact() {
+  const captchaRef = useRef(null);
+
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [message, setMessage] = useState("");
+  const [error, setError] = useState("");
+
+  // const validEmailRegex = RegExp(
+  //   /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+  // );
+
+  const verifyToken = async (token) => {
+    try {
+      const response = await axios.post(
+        `http://localhost:4000/verify-token`,
+        {
+          secret: process.env.REACT_APP_SECRET_KEY,
+          token
+        },
+        console.log(token)
+      );
+      return response.data;
+    } catch (error) {
+      console.log("error ", error);
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+    setMessage("");
+    if (firstName && lastName) {
+      const token = captchaRef.current.getValue();
+      if (token) {
+        const validToken = await verifyToken(token);
+        if (validToken.success) {
+          setMessage("Hurray!! you have submitted the form");
+        } else {
+          setError("Sorry!! Token invalid");
+        }
+      } else {
+        setError("You must confirm you are not a robot");
+      }
+    } else {
+      setError("First name and Last name are required");
+    }
+  };
+
   return (
     <main id="contact">
       <Helmet>
         <title>CONTACT | Les Amis de Sainte Madeleine de La Jarrie</title>
       </Helmet>
 
-      <form name="contact" method="post">
-        <input type="hidden" name="form-name" value="contact" />
-        <p>
-          <label>
-            Prénom: <input type="text" name="firstname" />
-          </label>
-        </p>
-        <p>
-          <label>
-            Nom: <input type="text" name="lastname" />
-          </label>
-        </p>
-        <p>
-          <label>
-            Email: <input type="email" name="email" />
-          </label>
-        </p>
-        <p>
-          <label>
-            Message: <textarea name="message" />
-          </label>
-        </p>
-        <p>
-          <button type="submit">Envoyer</button>
-        </p>
-      </form>
+      <div className="form">
+        {error && <p className="textError">Error!! {error}</p>}
+        {message && <p className="textSuccess">Success!! {message}</p>}
+        <form onSubmit={handleSubmit} className="formContainer">
+          <div className="formGroup">
+            <label htmlFor="firstName">Prénom</label>
+            <input
+              type="text"
+              name="first_name"
+              placeholder="Entrer votre prénom"
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+          </div>
+          <div className="formGroup">
+            <label htmlFor="lastName">Nom</label>
+            <input
+              type="text"
+              name="last_name"
+              placeholder="Entrer votre nom"
+              onChange={(e) => setLastName(e.target.value)}
+            />
+          </div>
+          <div className="formGroup">
+            <ReCAPTCHA sitekey={process.env.REACT_APP_SITE_KEY} ref={captchaRef} />
+          </div>
+          <div className="formGroup">
+            <button type="submit">Envoyer</button>
+          </div>
+        </form>
+      </div>
 
       <iframe
         title="map"
