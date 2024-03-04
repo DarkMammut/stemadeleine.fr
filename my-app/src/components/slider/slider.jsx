5771563ab7b1e251a1a8fb04f3b104b1bca4bef9
Sébastien BELLANGER <seb.bellanger@gmail.com>
2024-03-04 18:27:53 +0100

    new slider

--- my-app/src/components/slider/slider.jsx
+++ my-app/src/components/slider/slider.jsx
@@ -1,83 +1,48 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
+import Slider from "react-touch-drag-slider";
 import { FaChevronRight, FaChevronLeft } from "react-icons/fa";
-import Draggable from "react-draggable"; // Import the Draggable component
 import "./slider.scss";
 
 function ImageSlider({ slidesImages, openSlider, startSlide }) {
-  const url = process.env.PUBLIC_URL;
+  const publicUrl = process.env.PUBLIC_URL;
   const [appear, setAppear] = useState(0);
-  const [sliderData, setSliderData] = useState(slidesImages[0]);
-  const [sliderPosition, setSliderPosition] = useState(0);
-  const scrollContainerRef = useRef(null); // Create a ref for the scroll container
+  const [index, setIndex] = useState(0);
 
-  const handleClick = (index) => {
-    const slider = slidesImages[index];
-    setSliderData(slider);
-
-    // Calculate the position to center the clicked thumbnail
-    const thumbnailWidth = 100; // Adjust as needed based on your design
-    const containerWidth = slidesImages.length * thumbnailWidth;
-    const visibleWidth = 1224; // Adjust as needed based on your design
-    const centerOffset = (containerWidth - visibleWidth) / 2;
-    const centeredPosition = -index * thumbnailWidth + centerOffset;
-    setSliderPosition(centeredPosition);
+  const setFinishedIndex = (i) => {
+    setIndex(i);
   };
 
   useEffect(() => {
     if (openSlider === 1) {
       setAppear(1);
-      const slider = slidesImages[startSlide];
-      setSliderData(slider);
+      setIndex(startSlide);
     }
   }, [openSlider]);
 
   const handleNext = () => {
-    let nextIndex = 0;
-    const currentIndex = slidesImages.findIndex((slide) => slide === sliderData);
-    if (currentIndex === slidesImages.length - 1) {
-      nextIndex = 0;
+    if (index < slidesImages.length - 1) {
+      setIndex(index + 1);
     } else {
-      nextIndex = currentIndex + 1;
+      setIndex(0);
     }
-    const nextSlide = slidesImages[nextIndex];
-    setSliderData(nextSlide);
   };
 
   const handlePrevious = () => {
-    let previousIndex = 0;
-    const currentIndex = slidesImages.findIndex((slide) => slide === sliderData);
-    if (currentIndex === 0) {
-      previousIndex = slidesImages.length - 1;
+    if (index > 0) {
+      setIndex(index - 1);
     } else {
-      previousIndex = currentIndex - 1;
+      setIndex(slidesImages.length - 1);
     }
-    const previousSlide = slidesImages[previousIndex];
-    setSliderData(previousSlide);
   };
 
-  const handleKeyDown = (e, index) => {
-    if (e.key === "Enter") {
-      handleClick(index);
-    } else if (e.key === "Escape") {
+  const handleKeyDown = (e) => {
+    if (e.key === "Escape") {
       setAppear(0);
     } else if (e.key === "ArrowLeft") {
       handlePrevious();
     } else if (e.key === "ArrowRight") {
       handleNext();
     }
-  };
-
-  const handleDrag = (e, { deltaX }) => {
-    const newSliderPosition = sliderPosition + deltaX;
-
-    // Define your limits (in pixels)
-    const minPosition = -200; // Adjust as needed
-    const maxPosition = 200; // Adjust as needed
-
-    // Ensure the new position stays within limits
-    const clampedPosition = Math.max(minPosition, Math.min(maxPosition, newSliderPosition));
-
-    setSliderPosition(clampedPosition);
   };
 
   return (
@@ -92,7 +57,7 @@ function ImageSlider({ slidesImages, openSlider, startSlide }) {
         <div className="slider__shut__line" />
       </button>
       <div className="slider__backgroung" />
-      <div className="slider__container">
+      <div className="slider__container d-flex justify-content-center">
         <button
           className="slider__container__btn slider__container__btn--next no-style-btn d-flex"
           type="button"
@@ -109,31 +74,37 @@ function ImageSlider({ slidesImages, openSlider, startSlide }) {
           aria-label="button to go to the previous slide">
           <FaChevronLeft className="slider__container__btn__chevrons" />
         </button>
-        <img
-          className="slider__container__activeslide"
-          src={url + sliderData.url}
-          alt={sliderData.alt}
-        />
-      </div>
-      <div className="slider__thumbnails">
-        <Draggable axis="x" onDrag={handleDrag} nodeRef={scrollContainerRef}>
-          <div
-            ref={scrollContainerRef} // Attach the ref to the div element
-            className="scroll-container d-flex align-items-center"
-            style={{ transform: `translateX(${sliderPosition}px)` }}>
-            {slidesImages.map((image, i) => (
-              <div className="slider__thumbnails__thumbnail" key={image.id}>
-                <button
-                  className={sliderData.id === image.id ? "clicked no-style-btn" : "no-style-btn"}
-                  type="button"
-                  onKeyDown={() => handleKeyDown(i)}
-                  onClick={() => handleClick(i)}>
-                  <img src={url + image.url} alt={image.alt} />
-                </button>
-              </div>
+        <div className="slider__container__frame">
+          <Slider
+            onSlideComplete={setFinishedIndex}
+            activeIndex={index}
+            threshHold={100}
+            transition={0.2}
+            scaleOnDrag>
+            {slidesImages.map(({ id, url, alt }) => (
+              <img src={publicUrl + url} key={id} alt={alt} />
             ))}
-          </div>
-        </Draggable>
+          </Slider>
+        </div>
+        <ul className="slider__container__dots d-flex justify-content-center">
+          {slidesImages.map(({ id, url }, i) => (
+            <li
+              className={
+                i === index ? "slider__container__dots__dot active" : "slider__container__dots__dot"
+              }
+              key={id}>
+              <button
+                className="slider__container__dots__dot__btn no-style-btn"
+                type="button"
+                onClick={() => setIndex(i)}
+                aria-label="button to go to the selected slide"
+                style={{
+                  backgroundImage: `url(${publicUrl}${url})`
+                }}
+              />
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
